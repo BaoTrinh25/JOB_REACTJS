59d0eb8a1a675d3888ff46e8ad5efba49a4f8b26
BaoTrinh25 <109272775+BaoTrinh25@users.noreply.github.com>
2024-07-15 15:19:40 +0700

    Update Register.js

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,121 +1,136 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useNavigate  } from "react-router-dom";
+import { MyDispatchContext } from "../../configs/Context";
+import APIs, { endpoints } from "../../configs/APIs";
 
 const Register = () => {
-    const [username, setUsername] = useState("");
-    const [role, setRole] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
+
     const [error, setError] = useState("");
-    const nav = useNavigate (); 
+    const [loading, setLoading] = useState(false);
+    const nav = useNavigate(); 
 
-    const handleUsernameChange = (e) => {
-        setUsername(e.target.value);
+    const fields = [
+        { label: "Username", icon: "email", field: "username" },
+        { label: "Password", icon: "eye", field: "password", secureTextEntry: true },
+        { label: "Confirm password", icon: "eye", field: "confirm", secureTextEntry: true },
+    ];
+    
+    const [role, setRole] = useState("applicant");
+    const [user, setUser] = useState({});
+    const dispatch = useContext(MyDispatchContext);
+
+    const change = (value, field) => {
+        setUser((current) => {
+        return { ...current, [field]: value };
+        });
     };
 
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
-
-    const handleConfirmPasswordChange = (e) => {
-        setConfirmPassword(e.target.value);
-    };
+  //Hàm đăng kí
+   
 
     const handleRoleChange = (e) => {
-        setRole(e.target.value);
+        const value = e.target.value;
+
+        const rolevalue = value === "applicant" ? 0 : 1;
+
+        setRole(rolevalue)
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        // Kiểm tra mật khẩu có ít nhất 5 ký tự và chứa ít nhất một chữ cái và một chữ số
-        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/;
-
-        if (!password.match(passwordRegex)) {
-            setError(
-                "Mật khẩu phải có ít nhất 5 ký tự và chứa ít nhất một chữ cái và một chữ số"
-            );
-        } else if (password !== confirmPassword) {
-            setError("Mật khẩu không khớp");
-        } else if (!role) {
-            setError("Vui lòng chọn vai trò");
-        } else {
-            setError("");
             // Xử lý logic đăng ký ở đây (có thể gửi dữ liệu lên server)
+                if (user.password !== user.confirm) 
+                    setError("Mật khẩu không khớp");
+                else {
+                    setError("");
+                    setLoading(true);
+                try {
+                    let form = new FormData();
+                    for (let f in user) {
+                        form.append(f, user[f]);
+                    }
+                    form.append("role", role)
+                    let res = await APIs.post(endpoints["register"], form, {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                    });
+                    if (res.status === 201) {
+                        nav('/login');
+                    }
+                } catch (error) {
+                    console.error(error);
+                } finally {
+                    setLoading(false);
+                }
             alert("Đăng ký thành công");
 
-            switch (role) {
-                case 'applicant':
-                    nav('/register-applicant');
-                    break;
-                case 'employer':
-                    nav('/register-employer');
-                    break;
-                case 'admin':
-                    nav('/');
-                    break;
-                default:
-                    break;
-            }
+            // switch (role) {
+            //     case 'applicant':
+            //         nav('/register-applicant');
+            //         break;
+            //     case 'employer':
+            //         nav('/register-employer');
+            //         break;
+            //     case 'admin':
+            //         nav('/');
+            //         break;
+            //     default:
+            //         break;
+            // }
             
         }
     };
 
+    // const [selectedImage, setSelectedImage] = useState(null);
+
+    // const handleImageSelect = (event) => {
+    //     const file = event.target.files[0];
+    //     const reader = new FileReader();
+
+    //     reader.onload = (e) => {
+    //     const imageDataURL = e.target.result;
+    //     setSelectedImage(imageDataURL);
+    //     };
+
+    //     reader.readAsDataURL(file);
+    // };
+
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center items-center h-screen pt-10">
             <form
                 className="bg-slate-200 p-8 rounded shadow-md w-80"
                 onSubmit={handleSubmit}
             >
                 <h2 className="text-2xl font-bold mb-4 text-center">REGISTER</h2>
-                <div className="mb-4">
-                    <label
-                        className="block text-gray-700 text-sm font-bold mb-2"
-                        htmlFor="username"
-                    >
-                        User Name
-                    </label>
-                    <input
-                        type="text"
-                        id="username"
+
+                {fields.map((f) => (
+                    <div key={f.field} className="mb-5">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={f.field}>{f.label}</label>
+                        <input
+                        type={f.secureTextEntry ? "password" : "text"}
+                        id={f.field}
                         className="w-full px-3 py-2 border rounded"
-                        value={username}
-                        onChange={handleUsernameChange}
+                        value={user[f.field] || ""} // Thêm || "" để tránh hiển thị undefined khi giá trị chưa được định nghĩa
+                        onChange={(event) => change(event.target.value, f.field)}// Chỉ truyền giá trị event.target.value
                         required
-                    />
-                </div>
-                <div className="mb-4">
-                    <label
-                        className="block text-gray-700 text-sm font-bold mb-2"
-                        htmlFor="password"
-                    >
-                        Password
+                        />
+                    </div>
+                ))}
+                {/* <div class="block items-center mb-5">
+                    <label class="bg-blue-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+                        <span>Select an Image</span>
+                        <input type="file" accept="image/*" onChange={handleImageSelect} class="hidden" />
                     </label>
-                    <input
-                        type="password"
-                        id="password"
-                        className="w-full px-3 py-2 border rounded"
-                        value={password}
-                        onChange={handlePasswordChange}
-                        required
-                    />
-                </div>
-                <div className="mb-4">
-                    <label
-                        className="block text-gray-700 text-sm font-bold mb-2"
-                        htmlFor="confirmPassword"
-                    >
-                        Confirm Password
-                    </label>
-                    <input
-                        type="password"
-                        id="confirmPassword"
-                        className="w-full px-3 py-2 border rounded"
-                        value={confirmPassword}
-                        onChange={handleConfirmPasswordChange}
-                        required
-                    />
-                </div>
+
+                    {selectedImage && (
+                        <div class="flex mt-4">
+                        <img src={selectedImage} alt="Selected" class="max-w-[25%]" />
+                        </div>
+                    )}
+                    
+                </div> */}
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2">
                         Choose Role
@@ -127,7 +142,7 @@ const Register = () => {
                                 className="form-radio"
                                 name="role"
                                 value="applicant"
-                                checked={role === "applicant"}
+                                checked={role === 0}
                                 onChange={handleRoleChange}
                                 required
                             />
@@ -139,7 +154,7 @@ const Register = () => {
                                 className="form-radio"
                                 name="role"
                                 value="employer"
-                                checked={role === "employer"}
+                                checked={role === 1}
                                 onChange={handleRoleChange}
                                 required
                             />
@@ -151,6 +166,7 @@ const Register = () => {
                 <button
                     type="submit"
                     className="w-full bg-teal-700 text-white py-2 rounded hover:bg-teal-900"
+                    loading={loading}
                 >
                     Register
                 </button>
